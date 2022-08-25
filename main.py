342ad059faca652a12b81b35878c539c8901592b
Jeremy Pulvar <pulvar.jeremy@icloud.com>
2022-08-25 23:50:37 +0200

    Version 1.0.2 - Ajout d'un timer pour éviter le ban IP

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from scrape_page import scrape_book_infos
 from export_csv import export_csv
 from category import *
 import os
+import time
 
 list_of_category = []
 list_of_category = get_all_category("http://books.toscrape.com/index.html", list_of_category)
@@ -9,8 +10,11 @@ list_of_category = get_all_category("http://books.toscrape.com/index.html", list
 for line in list_of_category:
     if not os.path.exists('./' + category_name(line)):
         os.makedirs('./' + category_name(line))
+    else:
+        continue
     list_of_book = []
     list_of_book = category(line, list_of_book)
     for url_book in list_of_book:
         book_infos = scrape_book_infos(url_book, category_name(line))
         export_csv(book_infos, category_name(line))
+    time.sleep(2)
