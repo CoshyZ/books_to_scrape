f253eeb104f54f2469946808a1b7185b798a51f2
Jeremy Pulvar <pulvar.jeremy@icloud.com>
2022-08-25 10:14:42 +0200

    remove comments in main.py

--- main.py
+++ main.py
@@ -11,9 +11,3 @@ for line in list_of_category:
     for url_book in list_of_book:
         book_infos = scrape_book_infos(url_book)
         export_csv(book_infos, category_name(line) + '.csv')
-
-#http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html
-#url_category = "http://books.toscrape.com/catalogue/category/books/academic_40/index.html"
-#Classic category : http://books.toscrape.com/catalogue/category/books/classics_6/index.html
-#Nonfiction http://books.toscrape.com/catalogue/category/books/nonfiction_13/page-3.html
-#Romance category http://books.toscrape.com/catalogue/category/books/romance_8/index.html
